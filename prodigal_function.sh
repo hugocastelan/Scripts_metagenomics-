b4f5ac2be4740bc840e76bcf49113a7a9f445651
Hugo Castelán Sánchez <64658071+hugocastelan@users.noreply.github.com>
2021-04-27 10:54:04 -0500

    Update prodigal_function.sh

--- prodigal_function.sh
+++ prodigal_function.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 
+# -*- coding: utf-8 -*- # NAME [prodigal_function.sh]	Version [1.0]
+# AUTHOR  Hugo Castelan Sanchez 
+# CREATED (2019)
+# USAGE prodigal_function.sh directory with assembly data 
 
+# DESCRIPTION
+# Performs structural annotation using prodigal from multiple fasta files
 
 
 for file in *.fa ;
