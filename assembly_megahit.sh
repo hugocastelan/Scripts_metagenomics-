82339e5e1d4db10836a083a920a8efee04609ec6
Hugo Castelán Sánchez <64658071+hugocastelan@users.noreply.github.com>
2021-04-27 10:47:35 -0500

    Update assembly_megahit.sh

--- assembly_megahit.sh
+++ assembly_megahit.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# -*- coding: utf-8 -*- # NAME [assembly_megahit.sh]	Version [1.0]
+# AUTHOR  Hugo Castelan Sanchez 
+# CREATED (2019)
+# USAGE assembly_megahit.sh directory with fastq
+
+# DESCRIPTION
+# Assembly multiple fastq 
+
+
 for  f1 in *_1.fq 
  do           
  f2=${fi%%_1.fq}"_2.fq"
