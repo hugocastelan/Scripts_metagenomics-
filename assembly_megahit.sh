767c19bd34fec9b2a3b6a86c13d1ccde2c822fad
Hugo Castelán Sánchez <64658071+hugocastelan@users.noreply.github.com>
2021-04-27 11:14:15 -0500

    Update assembly_megahit.sh

--- assembly_megahit.sh
+++ assembly_megahit.sh
@@ -12,6 +12,6 @@
 for  f1 in *_1.fq 
  do           
  f2=${fi%%_1.fq}"_2.fq"
- /home/sonia/megahit/megahit  -1  $f1  -2 $f2 -o "assembly$fi.out" 
+ megahit  -1  $f1  -2 $f2 -o "assembly$fi.out" 
  done
 
